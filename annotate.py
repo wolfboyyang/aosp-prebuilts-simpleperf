b7f37d2721d82fc636ac1b4ee9b9d1fad70c7ee7
Yabin Cui <yabinc@google.com>
2017-05-08 11:48:55 -0700

    Update NDK prebuilts to build 3984832.
    Taken from branch aosp-master.
    
    Update update.py to copy libwinpthread-1.dll.
    
    Bug: http://b/37788631
    Test: none.
    
    Change-Id: I73ddcea57e64a91b431a5f28528ed26d09d37cbf

--- annotate.py
+++ annotate.py
@@ -106,13 +106,16 @@ class Addr2Line(object):
             out_pos += 1
             assert addr_line[:2] == "0x"
             assert out_pos < len(stdoutdata)
-            assert addrs[addr_pos] == int(addr_line, 16)
             source_lines = []
             while out_pos < len(stdoutdata) and stdoutdata[out_pos][:2] != "0x":
                 function = stdoutdata[out_pos]
                 out_pos += 1
                 assert out_pos < len(stdoutdata)
-                file, line = stdoutdata[out_pos].split(':')
+                # Handle lines like "C:\Users\...\file:32".
+                items = stdoutdata[out_pos].rsplit(':', 1)
+                if len(items) != 2:
+                    continue
+                (file, line) = items
                 line = line.split()[0]  # Remove comments after line number
                 out_pos += 1
                 if file.find('?') != -1:
@@ -124,8 +127,8 @@ class Addr2Line(object):
                 else:
                     line = int(line)
                 source_lines.append(SourceLine(file, function, line))
-                dso[addrs[addr_pos]] = source_lines
-                addr_pos += 1
+            dso[addrs[addr_pos]] = source_lines
+            addr_pos += 1
         assert addr_pos == len(addrs)
 
 
@@ -558,6 +561,9 @@ class SourceFileAnnotator(object):
                 path = key
                 from_path = path
                 to_path = os.path.join(dest_dir, path[1:])
+            elif is_windows() and key.find(':\\') != -1 and os.path.isfile(key):
+                from_path = key
+                to_path = os.path.join(dest_dir, key.replace(':\\', '\\'))
             else:
                 path = key[1:] if key.startswith('/') else key
                 # Change path on device to path on host
@@ -607,7 +613,10 @@ class SourceFileAnnotator(object):
             for line in range(1, len(lines) + 1):
                 annotate = annotates.get(line)
                 if annotate is None:
-                    annotate = empty_annotate
+                    if not lines[line-1].strip():
+                        annotate = ''
+                    else:
+                        annotate = empty_annotate
                 else:
                     annotate = '/* ' + annotate + (
                         ' ' * (max_annotate_cols - len(annotate))) + ' */'
@@ -624,3 +633,4 @@ if __name__ == '__main__':
     config = load_config(args.config)
     annotator = SourceFileAnnotator(config)
     annotator.annotate()
+    log_info('annotate finish successfully, please check result in annotated_files/.')
